8053446f3009a2a59aa95ef8b3555afbffe9cfae
djgosnell@gmail.com <djgosnell@gmail.com@88abc154-7f6d-4d4e-a7f2-d1572fda52b6>
2011-03-01 19:18:55 +0000

    Removed old files.

--- dtxCore/Properties/BuildInformation.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-
-// Major.Minor.Build(Total days since start of project)
-// Project begin date: Monday, January 31, 2011 5:20:00 PM
-[assembly: AssemblyVersion("0.2.22")]
-[assembly: AssemblyFileVersion("0.2.22")]
-[assembly: AssemblyCompany("Dtronix")]
-[assembly: AssemblyCopyright("©2011 Dtronix")]
