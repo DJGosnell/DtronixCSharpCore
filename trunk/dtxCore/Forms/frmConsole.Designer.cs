ce2bf1975ba9e373b2a3d84e90970a6203d699f8
djgosnell@gmail.com <djgosnell@gmail.com@88abc154-7f6d-4d4e-a7f2-d1572fda52b6>
2011-03-03 19:34:45 +0000

    Added form to allow output of information for debugging.

---
+++ trunk/dtxCore/Forms/frmConsole.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace dtxCore.Forms {
+	partial class frmConsole {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if(disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this._rtbOutput = new System.Windows.Forms.RichTextBox();
+			this.SuspendLayout();
+			// 
+			// _rtbOutput
+			// 
+			this._rtbOutput.BackColor = System.Drawing.Color.Black;
+			this._rtbOutput.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this._rtbOutput.DetectUrls = false;
+			this._rtbOutput.Dock = System.Windows.Forms.DockStyle.Fill;
+			this._rtbOutput.Font = new System.Drawing.Font("Lucida Console", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this._rtbOutput.ForeColor = System.Drawing.Color.White;
+			this._rtbOutput.Location = new System.Drawing.Point(0, 0);
+			this._rtbOutput.Name = "_rtbOutput";
+			this._rtbOutput.ReadOnly = true;
+			this._rtbOutput.Size = new System.Drawing.Size(612, 333);
+			this._rtbOutput.TabIndex = 0;
+			this._rtbOutput.Text = "Loading...";
+			// 
+			// frmConsole
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.Black;
+			this.ClientSize = new System.Drawing.Size(612, 333);
+			this.Controls.Add(this._rtbOutput);
+			this.Name = "frmConsole";
+			this.ShowIcon = false;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Console Output";
+			this.Load += new System.EventHandler(this.frmConsole_Load);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.RichTextBox _rtbOutput;
+
+	}
+}
---
+++ trunk/dtxCore/Forms/frmConsole.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace dtxCore.Forms {
+	public partial class frmConsole : Form {
+		public frmConsole() {
+			InitializeComponent();
+		}
+
+		private void frmConsole_Load(object sender, EventArgs e) {
+			_rtbOutput.Text = "";
+		}
+
+		public void writeLine(string text) {
+			writeLine(text, "white");
+		}
+
+		public void writeLine(string text, string color) {
+			this.Invoke((MethodInvoker)delegate {
+				int current_len = _rtbOutput.TextLength;
+				_rtbOutput.AppendText(text);
+				_rtbOutput.AppendText("\n");
+
+				_rtbOutput.Select(current_len, text.Length + 1);
+				_rtbOutput.SelectionColor = Color.FromName(color);
+				_rtbOutput.DeselectAll();
+			});
+		}
+
+		public void write(string text) {
+			write(text, "white");
+		}
+
+		public void write(string text, string color) {
+			this.Invoke((MethodInvoker)delegate {
+				int current_len = _rtbOutput.TextLength;
+				_rtbOutput.AppendText(text);
+
+				_rtbOutput.Select(current_len, text.Length);
+				_rtbOutput.SelectionColor = Color.FromName(color);
+				_rtbOutput.DeselectAll();
+			});
+		}
+
+	}
+}
